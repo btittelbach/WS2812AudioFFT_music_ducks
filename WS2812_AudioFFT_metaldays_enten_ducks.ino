c0b338afe6df0f4abb66c43ce1b21e8e53b38ae3
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-07-18 01:07:02 +0200

    cleaner animation handling with classes

--- WS2812_AudioFFT_metaldays_enten_ducks.ino
+++ WS2812_AudioFFT_metaldays_enten_ducks.ino
@@ -14,12 +14,6 @@
 /// * https://github.com/PaulStoffregen/WS2812Serial
 ///
 
-// How many leds are in the strip?
-#define NUM_LEDS 150
-
-typedef uint32_t ledctr_t;
-typedef unsigned long millis_t;
-
 // Data pin that led data will be written out over
 #define WS2812_PIN 5
 
@@ -33,10 +27,10 @@ typedef unsigned long millis_t;
 #define PHOTORESISTOR_PIN 17
 //#define PHOTORESISTOR_USE_ADC
 
+#define NUM_LEDS 150
 #define BUTTON_DEBOUNCE  400
 #define LIGHT_THRESHOLD (500*3300/4096)  //500mV
 #define LIGHT_DEBOUNCE 30000
-
 
 #define EEPROM_CURRENT_VERSION 0
 #define EEPROM_ADDR_VERS 0
@@ -70,27 +64,48 @@ AudioConnection          patchCord4(adc_stereo, 1, photoPeak, 0);
 
 #define FFT_SIZE 256
 
-typedef uint8_t animation_t;
-const animation_t ANIM_FFT_OCTAVES=0;
-const animation_t ANIM_FFT_OCTAVES_WHEN_DARK=1;
-const animation_t ANIM_PLASMA=2;
-const animation_t ANIM_PLASMA_WHEN_DARK=3;
-const animation_t ANIM_ELECTRIC_SPARKLES=4;
-const animation_t ANIM_ELECTRIC_SPARKLES_WHEN_DARK=5;
-const animation_t ANIM_GRAVITY_DOTS=6;
-const animation_t ANIM_GRAVITY_DOTS_WHEN_DARK=7;
-const animation_t ANIM_RMS_HUE=8;
-const animation_t ANIM_RMS_HUE_WHEN_DARK=9;
-const animation_t ANIM_STRIP_TEST=10;
-const animation_t ANIM_PHOTOSENSOR_TEST=11;
-const animation_t NUM_ANIM=12;
-
-animation_t animation_current_=ANIM_FFT_OCTAVES_WHEN_DARK;
+CRGB leds_[NUM_LEDS];
 bool is_dark_=true;
 float last_max_peak_=0.01;
+int32_t dark_count_=0;
+uint16_t light_level=0;
 
-// This is an array of leds.  One item for each led in your strip.
-CRGB leds_[NUM_LEDS];
+#include "animations.h"
+
+AnimationBlack anim_fade_to_black;
+AnimationPlasma anim_plasma;
+RunOnlyInDarkness anim_plasma_when_dark(anim_plasma, anim_fade_to_black);
+AnimationRMSHue anim_rms_hue;
+RunOnlyInDarkness anim_rms_hue_when_dark(anim_rms_hue, anim_fade_to_black);
+AnimationFFTOctaves anim_fft_octaves;
+RunOnlyInDarkness anim_fft_octaves_when_dark(anim_fft_octaves, anim_fade_to_black);
+AnimationGravityDots anim_gravity_dots;
+RunOnlyInDarkness anim_gravity_dots_when_dark(anim_gravity_dots, anim_fade_to_black);
+AnimationFireworks anim_fireworks;
+RunOnlyInDarkness anim_fireworks_when_dark(anim_fireworks, anim_fade_to_black);
+AnimationFullFFT anim_fft_full_and_boring;
+AnimationPhotosensorDebugging anim_photoresistor_debugging;
+AnimationStripTest anim_strip_debugging;
+
+BaseAnimation animations_list_[]=
+	{anim_fft_octaves
+	,anim_fft_octaves_when_dark
+	,anim_rms_hue
+	,anim_rms_hue_when_dark
+	,anim_plasma
+	,anim_plasma_when_dark
+	,anim_gravity_dots
+	,anim_gravity_dots_when_dark
+	,anim_fireworks
+	,anim_fireworks_when_dark
+	,anim_fft_full_and_boring
+	,anim_photoresistor_debugging
+	,anim_strip_debugging
+	};
+
+uint8_t animation_current_= 1;
+#define NUM_ANIM sizeof(animations_list_)/sizeof(BaseAnimation)
+
 
 // This function sets up the ledsand tells the controller about them
 void setup() {
@@ -125,8 +140,6 @@ void load_from_EEPROM()
 }
 
 
-int32_t dark_count_=0;
-uint16_t light_level=0;
 void task_check_lightlevel()
 {
 #ifdef PHOTORESISTOR_USE_ADC
@@ -165,195 +178,6 @@ inline void task_sample_mic()
 
 }
 
-millis_t animation_black()
-{
-	for (ledctr_t led=0; led<NUM_LEDS; led++)
-		leds_[led].fadeToBlackBy(80);
-	return 1000;
-}
-
-millis_t animation_fft_hue()
-{
-	if (!audioFFT.available())
-		return 2;
-	for (ledctr_t l=0; l<min(FFT_SIZE,NUM_LEDS);l++)
-	{
-		uint8_t v = static_cast<uint8_t>(audioFFT.read(l)*255.0);
-		CHSV x(v,128,60);
-		hsv2rgb_rainbow(x,leds_[l]);
-	}
-	return 10; //1ms max delay
-}
-
-#define NUM_OCTAVES  8 //log2(256)
-
-void fft_calc_octaves255(uint8_t led_octaves_magnitude[NUM_OCTAVES])
-{
-	led_octaves_magnitude[0] =  audioFFT.read(0) /last_max_peak_ * 0xff;
-    led_octaves_magnitude[1] =  audioFFT.read(1) /last_max_peak_ * 0xff;
-    led_octaves_magnitude[2] =  audioFFT.read(2,  3) /last_max_peak_ * 0xff;
-    led_octaves_magnitude[3] =  audioFFT.read(4,  7) /last_max_peak_ * 0xff;
-    led_octaves_magnitude[4] =  audioFFT.read(8,  16) /last_max_peak_ * 0xff;
-    led_octaves_magnitude[5] = audioFFT.read(17,  32) /last_max_peak_ * 0xff;
-    led_octaves_magnitude[6] = audioFFT.read(33, 64) /last_max_peak_ * 0xff;
-    led_octaves_magnitude[7] = audioFFT.read(65, 127) /last_max_peak_ * 0xff;
-}
-
-uint8_t get_fft_octaves_beat(uint8_t led_octaves_magnitude[NUM_OCTAVES])
-{
-	uint8_t beat=0;
-	for (uint8_t o=1; o<NUM_OCTAVES; o++)
-	{
-		beat += ((led_octaves_magnitude[0] > 180)? 1:0);
-	}
-	//again for last one, adding +2 in sum if beat.
-	beat += ((led_octaves_magnitude[NUM_OCTAVES-1] > 180)? 1:0);
-	return beat;
-}
-
-millis_t animation_fft_octaves()
-{
-	static uint8_t last_beat=0;
-	static ledctr_t led_shift=0;
-
-	const millis_t default_delay=10;
-
-	if (!audioFFT.available() || !audioPeak.available())
-		return 2;
-	uint8_t led_octaves_magnitude[NUM_OCTAVES];
-	//calc octaves magnitude
-	fft_calc_octaves255(led_octaves_magnitude);
-	last_max_peak_ = max(last_max_peak_,audioPeak.read());
-	uint8_t beat = get_fft_octaves_beat(led_octaves_magnitude);
-
-	//set brightness depending on beat
-    if (beat >= 7)
-    {
-		fill_solid(leds_, NUM_LEDS, CRGB::Gray);
-        FastLED.setBrightness(120);
-        return default_delay;
-    } else if (last_beat != beat)
-    {
-        FastLED.setBrightness( 40+beat*beat*5 );
-        last_beat = beat;
-	}
-
-	const ledctr_t start_octave = 0;
-	const ledctr_t spectr_width = (NUM_OCTAVES-start_octave)*2-1; //8 + 7 in between = 15
-	const ledctr_t spectr_repetitions = NUM_LEDS / spectr_width;
-
-	for (ledctr_t repetition=0; repetition<spectr_repetitions; repetition++)
-	{
-		ledctr_t start_pos = spectr_width*repetition;
-		//paint octaves to every second pixel
-		for (ledctr_t o=start_octave; o < NUM_OCTAVES; o++)
-		{
-			hsv2rgb_rainbow(
-				CHSV(led_octaves_magnitude[o] + repetition*30,
-					constrain(led_octaves_magnitude[o]+30, 0,0xff),
-					led_octaves_magnitude[o])
-				,leds_[addmod8(start_pos+o*2,led_shift,NUM_LEDS)]
-			);
-		}
-
-		//interpolate pixels in between
-		for (ledctr_t o=start_octave; o < NUM_OCTAVES-1; o++)
-		{
-			ledctr_t pos_before = addmod8(start_pos+o*2,led_shift,NUM_LEDS);
-			ledctr_t pos_between = addmod8(pos_before+1,led_shift,NUM_LEDS);
-			ledctr_t pos_after   = addmod8(pos_before+2,led_shift,NUM_LEDS);
-			leds_[pos_between].r = (leds_[pos_before].r+leds_[pos_after].r) / 2;
-			leds_[pos_between].g = (leds_[pos_before].g+leds_[pos_after].g) / 2;
-			leds_[pos_between].b = (leds_[pos_before].b+leds_[pos_after].b) / 2;
-		}
-	}
-
-	if (beat > 0)
-	{
-		led_shift+=((beat+4)/2-2);
-	}
-
-	if (beat>3 && beat<7)
-        return default_delay*2;
-    else
-		return default_delay;
-}
-
-		// ledctr_t middle = oct*ledwith_octave + (ledwith_octave/2);
-		// ledctr_t width = fht_oct_out[oct] * ledwith_octave / (1<<(sizeof(int16_t)-1));
-		// uint8_t value = fht_oct_out[oct] * ledwith_octave / (1<<(sizeof(int16_t)-1));
-
-millis_t animation_striptest()
-{
-	static ledctr_t whiteLed=0;
-	// Turn our current led on to white, then show the leds
-	leds_[whiteLed+0] = CRGB::Black;
-	leds_[(whiteLed+1)%NUM_LEDS] = CRGB::Blue;
-	leds_[(whiteLed+2)%NUM_LEDS] = CRGB::Green;
-	leds_[(whiteLed+3)%NUM_LEDS] = CRGB::Red;
-	leds_[(whiteLed+4)%NUM_LEDS] = CRGB::White;
-	whiteLed++;
-	whiteLed %= NUM_LEDS;
-	return 100;
-}
-
-millis_t animation_photosensor_level()
-{
-	for (ledctr_t l=0; l<NUM_LEDS; l++)
-	{
-		leds_[l]=CRGB::Black;
-	}
-	ledctr_t light_level_mapped_onto_leds = (light_level*NUM_LEDS/4095);
-	for (ledctr_t l=0; l<light_level_mapped_onto_leds; l++)
-	{
-		leds_[l].b=30;
-	}
-	ledctr_t dark_count_mapped_onto_leds = min(NUM_LEDS, ((dark_count_+LIGHT_DEBOUNCE)*NUM_LEDS/(2*LIGHT_DEBOUNCE)));
-	for (ledctr_t l=0; l<dark_count_mapped_onto_leds; l++)
-	{
-		leds_[l].r=60;
-	}
-	if (is_dark_)
-	{
-		leds_[0].g=100;
-	}
-	return 100;
-}
-
-
-millis_t animation_audio_rms_hue()
-{
-	static uint8_t hue=0;
-	if (!audioRMS.available() || !audioPeak.available())
-		return 2;
-	float rms = audioRMS.read(); //0.0 ... 1.0
-	uint8_t audiopower = static_cast<uint8_t>(rms/last_max_peak_*0xff);
-	last_max_peak_ = max(last_max_peak_,audioPeak.read());
-	//move pattern forwards
-	for (ledctr_t l=NUM_LEDS-1; l>0; l--)
-	{
-		leds_[l]=leds_[l-1];
-	}
-	hsv2rgb_rainbow(CHSV(hue,128,audiopower),leds_[0]);
-	hue++;
-	return 4;
-}
-
-millis_t animation_plasma()
-{
-	static ledctr_t steps=0;
-	for (ledctr_t i=0;i<NUM_LEDS;i++)
-	{
-		uint16_t spani = (0x7f*(uint16_t)i)/(NUM_LEDS-1);
-		uint8_t s = quadwave8(add8(steps,mul8(spani,8)));
-		uint8_t u = quadwave8((-(steps<<1)-mul8(spani,3)+(quadwave8(spani<<1))));
-		leds_[i].r=s;
-		leds_[i].g=u;
-		leds_[i].b=0;
-	}
-	steps++;
-}
-
 void task_animate_leds()
 {
 	static millis_t next_run=0;
@@ -362,44 +186,20 @@ void task_animate_leds()
 	if (millis() < next_run)
 		return;
 
-	millis_t delay_ms=0;
-	switch (animation_current_)
-	{
-		default:
-		case ANIM_FFT_OCTAVES_WHEN_DARK:
-			if (is_dark_)
-				delay_ms=animation_fft_octaves();
-			else
-				delay_ms=animation_black();
-			break;
-		case ANIM_FFT_OCTAVES:
-			delay_ms=animation_fft_octaves();
-			break;
-		case ANIM_PLASMA_WHEN_DARK:
-			if (is_dark_)
-				delay_ms=animation_plasma();
-			else
-				delay_ms=animation_black();
-			break;
-		case ANIM_PLASMA:
-			delay_ms=animation_plasma();
-			break;
-		case ANIM_RMS_HUE:
-			delay_ms=animation_audio_rms_hue();
-			break;
-		case ANIM_ELECTRIC_SPARKLES:
-			delay_ms=animation_fft_hue(); //FIXME
-			break;
-		case ANIM_STRIP_TEST:
-			delay_ms=animation_striptest(); //FIXME
-			break;
-		case ANIM_PHOTOSENSOR_TEST:
-			delay_ms=animation_photosensor_level(); //FIXME
-			break;
-	}
+	//run current animation
+	millis_t delay_ms = animations_list_[animation_current_].run();
+
 	// Show the leds (only one of which is set to white, from above)
 	FastLED.show();
 	next_run=millis()+delay_ms;
+}
+
+void animation_switch_next()
+{
+	animation_current_++;
+	animation_current_%=NUM_ANIM;
+	save_to_EEPROM();
+	animations_list_[animation_current_].init();
 }
 
 void task_check_button()
@@ -415,12 +215,7 @@ void task_check_button()
 
 	if (btn_count_ == BUTTON_DEBOUNCE)
 	{
-		animation_current_++;
-		animation_current_%=NUM_ANIM;
-		save_to_EEPROM();
-		fill_solid(leds_, NUM_LEDS, CRGB::Black);
-		last_max_peak_=0.01;
-		FastLED.setBrightness(120);
+		animation_switch_next();
 	}
 }
 
---
+++ animations.h
@@ -0,0 +1,394 @@
+#ifndef ANIMATIONS_INCLUDE__H
+#define ANIMATIONS_INCLUDE__H
+
+#include <FastLED.h>
+
+typedef uint32_t ledctr_t;
+typedef unsigned long millis_t;
+
+
+class BaseAnimation
+{
+public:
+	virtual void init()
+	{
+		fill_solid(leds_, NUM_LEDS, CRGB::Black);
+		last_max_peak_=0.01;
+		FastLED.setBrightness(120);		
+	}
+
+	virtual millis_t run()
+	{
+		return 100;
+	}
+};
+
+class AnimationBlack : public BaseAnimation {
+public:
+	virtual millis_t run()
+	{
+		CPixelView<CRGB>(leds_,0,NUM_LEDS).fadeToBlackBy(60);
+		return 600;
+	}
+};
+
+// onlyInDarkness Decorator
+class RunOnlyInDarkness : public BaseAnimation {
+private:
+	BaseAnimation *dark_animation;
+	BaseAnimation *light_animation;
+public:
+	RunOnlyInDarkness(BaseAnimation &in_darkness, BaseAnimation &in_daylight) :dark_animation(&in_darkness), light_animation(&in_daylight) {}
+
+	virtual millis_t run()
+	{
+		if (is_dark_)
+			return dark_animation->run();
+		else 
+			return light_animation->run();
+	}
+};
+
+
+class AnimationPlasma : public BaseAnimation {
+private:
+	ledctr_t steps=0;
+
+public:
+	virtual millis_t run()
+	{
+		
+		for (ledctr_t i=0;i<NUM_LEDS;i++)
+		{
+			uint16_t spani = (0x7f*(uint16_t)i)/(NUM_LEDS-1);
+			uint8_t s = quadwave8(add8(steps,mul8(spani,8)));
+			uint8_t u = quadwave8((-(steps<<1)-mul8(spani,3)+(quadwave8(spani<<1))));
+			leds_[i].r=s;
+			leds_[i].g=u;
+			leds_[i].b=0;
+		}
+		steps++;
+		return 50;
+	}
+};
+
+class AnimationRMSHue : public BaseAnimation {
+private:
+	uint8_t hue=0;
+
+public:
+	virtual millis_t run()
+	{
+		if (!audioRMS.available() || !audioPeak.available())
+			return 2;
+		float rms = audioRMS.read(); //0.0 ... 1.0
+		uint8_t audiopower = static_cast<uint8_t>(rms/last_max_peak_*0xff);
+		last_max_peak_ = max(last_max_peak_,audioPeak.read());
+		//move pattern forwards
+		for (ledctr_t l=NUM_LEDS-1; l>0; l--)
+		{
+			leds_[l]=leds_[l-1];
+		}
+		hsv2rgb_rainbow(CHSV(hue,128,audiopower),leds_[0]);
+		hue++;
+		return 4;
+	}
+};
+
+class AnimationPhotosensorDebugging : public BaseAnimation {
+public:
+	virtual millis_t run()
+	{
+		for (ledctr_t l=0; l<NUM_LEDS; l++)
+		{
+			leds_[l]=CRGB::Black;
+		}
+		ledctr_t light_level_mapped_onto_leds = (light_level*NUM_LEDS/4095);
+		for (ledctr_t l=0; l<light_level_mapped_onto_leds; l++)
+		{
+			leds_[l].b=30;
+		}
+		ledctr_t dark_count_mapped_onto_leds = min(NUM_LEDS, ((dark_count_+LIGHT_DEBOUNCE)*NUM_LEDS/(2*LIGHT_DEBOUNCE)));
+		for (ledctr_t l=0; l<dark_count_mapped_onto_leds; l++)
+		{
+			leds_[l].r=60;
+		}
+		if (is_dark_)
+		{
+			leds_[0].g=100;
+		}
+		return 100;
+	}
+};
+
+class AnimationStripTest : public BaseAnimation {
+private:
+	ledctr_t whiteLed=0;
+
+public:
+	virtual millis_t run()
+	{
+		// Turn our current led on to white, then show the leds
+		leds_[whiteLed+0] = CRGB::Black;
+		leds_[(whiteLed+1)%NUM_LEDS] = CRGB::Blue;
+		leds_[(whiteLed+2)%NUM_LEDS] = CRGB::Green;
+		leds_[(whiteLed+3)%NUM_LEDS] = CRGB::Red;
+		leds_[(whiteLed+4)%NUM_LEDS] = CRGB::White;
+		whiteLed++;
+		whiteLed %= NUM_LEDS;
+		return 100;
+	}
+};
+
+#define NUM_OCTAVES  8 //log2(256)
+
+void fft_calc_octaves255(uint8_t led_octaves_magnitude[NUM_OCTAVES])
+{
+	led_octaves_magnitude[0] =  audioFFT.read(0) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[1] =  audioFFT.read(1) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[2] =  audioFFT.read(2,  3) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[3] =  audioFFT.read(4,  7) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[4] =  audioFFT.read(8,  16) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[5] = audioFFT.read(17,  32) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[6] = audioFFT.read(33, 64) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[7] = audioFFT.read(65, 127) /last_max_peak_ * 0xff;
+}
+
+uint8_t get_fft_octaves_beat(uint8_t led_octaves_magnitude[NUM_OCTAVES])
+{
+	uint8_t beat=0;
+	for (uint8_t o=1; o<NUM_OCTAVES; o++)
+	{
+		beat += ((led_octaves_magnitude[0] > 180)? 1:0);
+	}
+	//again for last one, adding +2 in sum if beat.
+	beat += ((led_octaves_magnitude[NUM_OCTAVES-1] > 180)? 1:0);
+	return beat;
+}
+
+
+class AnimationFFTOctaves : public BaseAnimation {
+private:
+	uint8_t last_beat=0;
+	ledctr_t led_shift=0;
+
+public:
+	virtual millis_t run()
+	{
+		const millis_t default_delay=10;
+
+		if (!audioFFT.available() || !audioPeak.available())
+			return 2;
+		uint8_t led_octaves_magnitude[NUM_OCTAVES];
+		//calc octaves magnitude
+		fft_calc_octaves255(led_octaves_magnitude);
+		last_max_peak_ = max(last_max_peak_,audioPeak.read());
+		uint8_t beat = get_fft_octaves_beat(led_octaves_magnitude);
+
+		//set brightness depending on beat
+	    if (beat >= 7)
+	    {
+			fill_solid(leds_, NUM_LEDS, CRGB::Gray);
+	        FastLED.setBrightness(120);
+	        return default_delay;
+	    } else if (last_beat != beat)
+	    {
+	        FastLED.setBrightness( 40+beat*beat*5 );
+	        last_beat = beat;
+		}
+
+		const ledctr_t start_octave = 0;
+		const ledctr_t spectr_width = (NUM_OCTAVES-start_octave)*2-1; //8 + 7 in between = 15
+		const ledctr_t spectr_repetitions = NUM_LEDS / spectr_width;
+
+		for (ledctr_t repetition=0; repetition<spectr_repetitions; repetition++)
+		{
+			ledctr_t start_pos = spectr_width*repetition;
+			//paint octaves to every second pixel
+			for (ledctr_t o=start_octave; o < NUM_OCTAVES; o++)
+			{
+				hsv2rgb_rainbow(
+					CHSV(led_octaves_magnitude[o] + repetition*30,
+						constrain(led_octaves_magnitude[o]+30, 0,0xff),
+						led_octaves_magnitude[o])
+					,leds_[addmod8(start_pos+o*2,led_shift,NUM_LEDS)]
+				);
+			}
+
+			//interpolate pixels in between
+			for (ledctr_t o=start_octave; o < NUM_OCTAVES-1; o++)
+			{
+				ledctr_t pos_before = addmod8(start_pos+o*2,led_shift,NUM_LEDS);
+				ledctr_t pos_between = addmod8(pos_before+1,led_shift,NUM_LEDS);
+				ledctr_t pos_after   = addmod8(pos_before+2,led_shift,NUM_LEDS);
+				leds_[pos_between].r = (leds_[pos_before].r+leds_[pos_after].r) / 2;
+				leds_[pos_between].g = (leds_[pos_before].g+leds_[pos_after].g) / 2;
+				leds_[pos_between].b = (leds_[pos_before].b+leds_[pos_after].b) / 2;
+			}
+		}
+
+		if (beat > 0)
+		{
+			led_shift+=((beat+4)/2-2);
+		}
+
+		if (beat>3 && beat<7)
+	        return default_delay*2;
+	    else
+			return default_delay;
+	}
+};
+
+class AnimationFullFFT : public BaseAnimation {
+public:
+	virtual millis_t run()
+	{
+		if (!audioFFT.available())
+			return 2;
+		for (ledctr_t l=0; l<min(FFT_SIZE,NUM_LEDS);l++)
+		{
+			uint8_t v = static_cast<uint8_t>(audioFFT.read(l)*255.0);
+			CHSV x(v,128,60);
+			hsv2rgb_rainbow(x,leds_[l]);
+		}
+		return 10; //1ms max delay
+	}
+};
+
+class AnimationGravityDots : public BaseAnimation {
+private:
+	uint8_t static const num_dots=7;
+	CRGB dot_color[num_dots];
+	int8_t dot_pos[num_dots];
+	int8_t dot_speed[num_dots];
+
+	int8_t static const dot_gravity_factor = 2;
+	int8_t static const dot_gravity_limit = 4;
+	int8_t static const dot_max_speed = 5;
+	uint16_t zero_move_ticks_=0;
+
+public:
+	virtual void init()
+	{
+		BaseAnimation::init();
+		CHSV dothsv;
+		dothsv.v=128;
+		dothsv.s=0xFF;
+		dothsv.h=random8();
+		for (ledctr_t d=0; d<num_dots;d++)
+		{
+			hsv2rgb_rainbow(dothsv,dot_color[d]);
+			dothsv.h+=0xFF/num_dots;
+			dot_speed[d]=dot_max_speed-(int8_t)random8(0,dot_max_speed*2);
+			dot_pos[d]=random8(0,NUM_LEDS-1);
+		}
+		zero_move_ticks_=0;
+	}
+
+	virtual millis_t run()
+	{
+		//calc
+		for (ledctr_t d1=0; d1<num_dots;d1++)
+		{
+			for (ledctr_t d2=d1+1; d2<num_dots;d2++)
+			{
+				int8_t dot_distance = dot_pos[d1] - dot_pos[d2];
+				//accel/decel
+				if (abs8(dot_distance) < dot_gravity_limit)
+				{
+					int8_t gravity = dot_gravity_limit-(dot_distance*dot_distance/dot_gravity_limit) + (dot_distance==0)?+1-random8(0,2):0;
+					if (dot_distance < 0)
+					{
+						dot_speed[d1] += gravity;
+						dot_speed[d2] -= gravity;
+					} else {
+						dot_speed[d1] -= gravity;
+						dot_speed[d2] += gravity;
+					}
+				}
+			}
+		}
+
+
+		for(int i = 0; i < NUM_LEDS; i++)
+		{
+			leds_[i].r = 0; leds_[i].g = 0; leds_[i].b = 0;
+		}
+
+		ledctr_t zerospeed=0;
+
+		for (ledctr_t d=0; d<num_dots; d++)
+		{
+			dot_speed[d]=max(min(dot_speed[d],dot_max_speed),0-dot_max_speed);
+			dot_pos[d]+=dot_speed[d];
+			if (dot_pos[d] < 0)
+				dot_pos[d] += NUM_LEDS;
+			dot_pos[d]%=NUM_LEDS;
+
+			leds_[dot_pos[d]].r+=dot_color[d].r;
+			leds_[dot_pos[d]].g+=dot_color[d].g;
+			leds_[dot_pos[d]].b+=dot_color[d].b;
+
+			zerospeed+=(dot_speed[d]==0)?1:0;
+		}
+
+
+		if (zerospeed>0)
+		{
+			zero_move_ticks_+=zerospeed;
+		} else {
+			zero_move_ticks_=0;
+		}
+		if (zero_move_ticks_ > 1024)
+		{
+			this->init();
+		}
+		return 250;
+	}
+};
+
+class AnimationFireworks : public BaseAnimation {
+public:
+	virtual millis_t run()
+	{
+		uint8_t color = random8();
+		uint32_t prevLed, thisLed, nextLed;
+		bool triggered = random(6) == 3;
+
+		CPixelView<CRGB>(leds_,0,NUM_LEDS).nscale8(128); // reduce each LEDs brightness by half
+
+		// set brightness(i) = ((brightness(i-1)/4 + brightness(i+1)) / 4) + brightness(i)
+		for (ledctr_t i=0 + 1; i <NUM_LEDS; i++)
+		{
+			prevLed = (reinterpret_cast<uint32_t>(leds_[i-1].raw) >> 2) & 0x3F3F3F3F;
+			thisLed = reinterpret_cast<uint32_t>(leds_[i].raw);
+			nextLed = (reinterpret_cast<uint32_t>(leds_[i+1].raw) >> 2) & 0x3F3F3F3F;
+			leds_[i] = CRGB(prevLed + thisLed + nextLed);
+		}
+
+		if(!triggered)
+		{
+			for(ledctr_t i=0; i<max(1, NUM_LEDS/20); i++)
+			{
+				if(random(10) == 0)
+				{
+					hsv2rgb_rainbow(CHSV(color,128,0xff),leds_[0 + random(NUM_LEDS)]);
+				}
+			}
+		} else
+		{
+			for(ledctr_t i=0; i<max(1, NUM_LEDS/10); i++) {
+				hsv2rgb_rainbow(CHSV(color,128,0xff),leds_[0 + random(NUM_LEDS)]);
+			}
+		}
+		return 40;
+	}
+};
+
+
+// ledctr_t middle = oct*ledwith_octave + (ledwith_octave/2);
+// ledctr_t width = fht_oct_out[oct] * ledwith_octave / (1<<(sizeof(int16_t)-1));
+// uint8_t value = fht_oct_out[oct] * ledwith_octave / (1<<(sizeof(int16_t)-1));
+
+
+#endif //ANIMATIONS_INCLUDE__H
