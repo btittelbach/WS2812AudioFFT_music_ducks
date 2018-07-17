04f073981a187af2fa65683f6a15031b562343a5
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-07-17 07:37:17 +0200

    works

--- WS2812_AudioFFT_metaldays_enten_ducks.ino
+++ WS2812_AudioFFT_metaldays_enten_ducks.ino
@@ -17,34 +17,49 @@
 // How many leds are in the strip?
 #define NUM_LEDS 150
 
-typedef uint8_t ledctr_t;
+typedef uint32_t ledctr_t;
+typedef unsigned long millis_t;
 
 // Data pin that led data will be written out over
 #define WS2812_PIN 5
 
-#define BUTTON_PIN 11
+#define BUTTON_PIN 12
 
 #define LED_PIN 13
 
-#define MICROPHONE_AIN A0  //14
+#define MICROPHONE_AIN A2
 
-#define PHOTODIODE_AIN A3  //17
+#define PHOTORESISTOR_AIN A3
+#define PHOTORESISTOR_PIN 17
+//#define PHOTORESISTOR_USE_ADC
 
 #define BUTTON_DEBOUNCE  400
-#define LIGHT_THRESHOLD (300*3300/4096)  //300mV
+#define LIGHT_THRESHOLD (500*3300/4096)  //500mV
 #define LIGHT_DEBOUNCE 30000
 
 // GUItool: begin automatically generated code
-AudioInputAnalogStereo   adc_stereo(MICROPHONE_AIN, PHOTODIODE_AIN);          //xy=70.33332824707031,228.33334350585938
+#ifdef PHOTORESISTOR_USE_ADC
+AudioInputAnalogStereo   adc_stereo(MICROPHONE_AIN, PHOTORESISTOR_AIN);          //xy=70.33332824707031,228.33334350585938
+#else
+AudioInputAnalog   		 adc_stereo(MICROPHONE_AIN);          //xy=70.33332824707031,228.33334350585938
+#endif
 AudioAnalyzeRMS          audioRMS;           //xy=297.33331298828125,201.33334350585938
 AudioAnalyzeFFT256       audioFFT;       //xy=305.33331298828125,240.33334350585938
+AudioAnalyzePeak         audioPeak;
+#ifdef PHOTORESISTOR_USE_ADC
 AudioAnalyzePeak         photoPeak;          //xy=310.33331298828125,287.33331298828125
+#endif
+//AudioOutputAnalog        dac1;           //xy=329,47 --> DAC Pin
+//AudioOutputUSB           usb1;           //xy=220.3333282470703,342.3333282470703
 AudioConnection          patchCord1(adc_stereo, 0, audioRMS, 0);
 AudioConnection          patchCord2(adc_stereo, 0, audioFFT, 0);
-AudioConnection          patchCord3(adc_stereo, 1, photoPeak, 0);
-AudioOutputUSB           usb1;           //xy=220.3333282470703,342.3333282470703
-AudioConnection          patchCord4(adc_stereo, 0, usb1, 0);
-AudioConnection          patchCord5(adc_stereo, 1, usb1, 1);
+AudioConnection          patchCord3(adc_stereo, 0, audioPeak, 0);
+#ifdef PHOTORESISTOR_USE_ADC
+AudioConnection          patchCord4(adc_stereo, 1, photoPeak, 0);
+#endif
+//AudioConnection          patchCord5(adc_stereo, 0, usb1, 0);
+//AudioConnection          patchCord6(adc_stereo, 1, usb1, 1);
+//AudioConnection          patchCord7(adc_stereo, 0, dac1, 0);
 // GUItool: end automatically generated code
 
 #define FFT_SIZE 256
@@ -55,17 +70,19 @@ const animation_t ANIM_FFT=1;
 const animation_t ANIM_SPARKLES=2;
 const animation_t ANIM_STRIP_TEST=3;
 const animation_t ANIM_PHOTOSENSOR_TEST=4;
-const animation_t NUM_ANIM=5;
+const animation_t ANIM_RMS=5;
+const animation_t NUM_ANIM=6;
 
-animation_t animation_current_=ANIM_PHOTOSENSOR_TEST;
+animation_t animation_current_=ANIM_FFT_SPARKLES_WHEN_DARK;
 bool is_dark_=true;
+float last_max_peak_=0.01;
 
 // This is an array of leds.  One item for each led in your strip.
 CRGB leds_[NUM_LEDS];
 
 // This function sets up the ledsand tells the controller about them
 void setup() {
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds	
+	AudioMemory(12);
 	delay(2000);
 
 	FastLED.addLeds<WS2812SERIAL,WS2812_PIN,GRB>(leds_,NUM_LEDS);
@@ -73,8 +90,9 @@ void setup() {
 	pinMode(LED_PIN,OUTPUT);
 	digitalWrite(LED_PIN, LOW);
 	pinMode(BUTTON_PIN, INPUT_PULLUP);
-	pinMode(PHOTODIODE_AIN, INPUT);
-	pinMode(MICROPHONE_AIN, INPUT);
+	pinMode(PHOTORESISTOR_AIN, INPUT);
+	pinMode(PHOTORESISTOR_PIN, INPUT);
+	pinMode(MICROPHONE_AIN, INPUT);	
 	load_from_EEPROM();
 }
 
@@ -92,16 +110,20 @@ void load_from_EEPROM()
 /*
 analogRead() must not be used, because AudioInputAnalog is regularly accessing the ADC hardware. If both access the hardware at the same moment, analogRead() can end up waiting forever, which effectively crashes your program. 
 */
-int16_t dark_count_=0;
+int32_t dark_count_=0;
 uint16_t light_level=0;
 void task_check_lightlevel()
 {
+#ifdef PHOTORESISTOR_USE_ADC
 	if (!photoPeak.available())
 		return;
 
-	// uint16_t light_level = analogReadADC1(PHOTODIODE_AIN);
+	// uint16_t light_level = analogReadADC1(PHOTORESISTOR_AIN);
 	light_level = photoPeak.read()*4095;
 	if (light_level > LIGHT_THRESHOLD)
+#else
+	if (digitalRead(PHOTORESISTOR_PIN) == LOW)
+#endif		
 	{
 		//assume daylight
 		if (dark_count_ < LIGHT_DEBOUNCE) {
@@ -128,14 +150,14 @@ inline void task_sample_mic()
 
 }
 
-uint16_t animation_black()
+millis_t animation_black()
 {
 	for (ledctr_t led=0; led<NUM_LEDS; led++)
 		leds_[led].fadeToBlackBy(80);
 	return 1000;
 }
 
-uint16_t animation_fft_hue()
+millis_t animation_fft_hue()
 {
 	if (!audioFFT.available())
 		return 2;
@@ -152,23 +174,24 @@ uint16_t animation_fft_hue()
 
 void fft_calc_octaves255(uint8_t led_octaves_magnitude[NUM_OCTAVES])
 {
-	led_octaves_magnitude[0] =  audioFFT.read(0) * 0xff;
-    led_octaves_magnitude[1] =  audioFFT.read(1) * 0xff;
-    led_octaves_magnitude[2] =  audioFFT.read(2,  3) * 0xff;
-    led_octaves_magnitude[3] =  audioFFT.read(4,  7) * 0xff;
-    led_octaves_magnitude[4] =  audioFFT.read(8,  16) * 0xff;
-    led_octaves_magnitude[5] = audioFFT.read(17,  32) * 0xff;
-    led_octaves_magnitude[6] = audioFFT.read(33, 64) * 0xff;
-    led_octaves_magnitude[7] = audioFFT.read(65, 127) * 0xff;
+	led_octaves_magnitude[0] =  audioFFT.read(0) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[1] =  audioFFT.read(1) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[2] =  audioFFT.read(2,  3) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[3] =  audioFFT.read(4,  7) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[4] =  audioFFT.read(8,  16) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[5] = audioFFT.read(17,  32) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[6] = audioFFT.read(33, 64) /last_max_peak_ * 0xff;
+    led_octaves_magnitude[7] = audioFFT.read(65, 127) /last_max_peak_ * 0xff;
 }
 
-uint16_t animation_fft_octaves()
+millis_t animation_fft_octaves()
 {
-	if (!audioFFT.available())
+	if (!audioFFT.available() || !audioPeak.available())
 		return 2;
 	uint8_t led_octaves_magnitude[NUM_OCTAVES];
 	//calc octaves magnitude
 	fft_calc_octaves255(led_octaves_magnitude);
+	last_max_peak_ = max(last_max_peak_,audioPeak.read());
 
 	//paint octaves to every second pixel
 	for (ledctr_t o=0; o < NUM_OCTAVES; o++)
@@ -203,7 +226,7 @@ uint16_t animation_fft_octaves()
 		// ledctr_t width = fht_oct_out[oct] * ledwith_octave / (1<<(sizeof(int16_t)-1));
 		// uint8_t value = fht_oct_out[oct] * ledwith_octave / (1<<(sizeof(int16_t)-1));
 
-uint16_t animation_striptest()
+millis_t animation_striptest()
 {
 	static ledctr_t whiteLed=0;
 	// Turn our current led on to white, then show the leds
@@ -217,62 +240,68 @@ uint16_t animation_striptest()
 	return 100;
 }
 
-uint16_t animation_photosensor_level()
+millis_t animation_photosensor_level()
 {
 	for (ledctr_t l=0; l<NUM_LEDS; l++)
 	{
 		leds_[l]=CRGB::Black;
 	}
-	for (ledctr_t l=0; l<(light_level*NUM_LEDS/4095); l++)
+	ledctr_t light_level_mapped_onto_leds = (light_level*NUM_LEDS/4095);
+	for (ledctr_t l=0; l<light_level_mapped_onto_leds; l++)
 	{
-		leds_[l].b=60;
+		leds_[l].b=30;
 	}
-	for (ledctr_t l=0; l<((dark_count_+LIGHT_DEBOUNCE)*NUM_LEDS/(2*LIGHT_DEBOUNCE)); l++)
+	ledctr_t dark_count_mapped_onto_leds = min(NUM_LEDS, ((dark_count_+LIGHT_DEBOUNCE)*NUM_LEDS/(2*LIGHT_DEBOUNCE)));
+	for (ledctr_t l=0; l<dark_count_mapped_onto_leds; l++)
 	{
 		leds_[l].r=60;
 	}
+	if (is_dark_)
+	{
+		leds_[0].g=100;
+	}
 	return 100;
 }
 
 
-uint16_t animation_audio_rms()
+millis_t animation_audio_rms_hue()
 {
-	static uint32_t audiopower=0;
-	if (!audioRMS.available())
+	static uint8_t hue=0;
+	if (!audioRMS.available() || !audioPeak.available())
 		return 2;
 	float rms = audioRMS.read(); //0.0 ... 1.0
-	audiopower+=static_cast<uint32_t>(rms*20);
-	for (ledctr_t l=NUM_LEDS-1; l>0; l++)
+	uint8_t audiopower = static_cast<uint8_t>(rms/last_max_peak_*0xff);
+	last_max_peak_ = max(last_max_peak_,audioPeak.read());
+	//move pattern forwards
+	for (ledctr_t l=NUM_LEDS-1; l>0; l--)
 	{
 		leds_[l]=leds_[l-1];
 	}
-	if (audiopower > 0)
-	{
-		leds_[0]=CRGB::Orange;
-		audiopower--;
-	} else {
-		leds_[0]=CRGB::Black;
-	}
-	return 100;
+	hsv2rgb_rainbow(CHSV(hue,128,audiopower),leds_[0]);
+	hue++;
+	return 4;
 }
-
 
 void task_animate_leds()
 {
-	static uint32_t next_run=0;
+	static millis_t next_run=0;
 
+	//ignore overflow for now, as the only thing that happens that we continue immediately once.
 	if (millis() < next_run)
 		return;
 
-	uint32_t delay_ms=0;
+	millis_t delay_ms=0;
 	switch (animation_current_)
 	{
 		default:
 		case ANIM_FFT_SPARKLES_WHEN_DARK:
 			if (is_dark_)
-				delay_ms=animation_audio_rms(); //FIXME
+				delay_ms=animation_striptest(); //FIXME
 			else
 				delay_ms=animation_black();
+			break;
+		case ANIM_RMS:
+			delay_ms=animation_audio_rms_hue();
 			break;
 		case ANIM_FFT:
 			delay_ms=animation_fft_hue(); //FIXME
@@ -310,6 +339,7 @@ void task_check_button()
 		save_to_EEPROM();
 		for (ledctr_t led=0; led<NUM_LEDS; led++)
 			leds_[led] = CRGB::Black;
+		last_max_peak_=0.01;
 	}
 }
 
