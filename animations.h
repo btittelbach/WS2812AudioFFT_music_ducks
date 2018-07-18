43380657fffde422bfd946de77c88d6b04bb293d
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-07-18 05:37:21 +0200

    make fire2012 cozy

--- animations.h
+++ animations.h
@@ -474,13 +474,19 @@ public:
 // COOLING: How much does the air cool as it rises?
 // Less cooling = taller flames.  More cooling = shorter flames.
 // Default 50, suggested range 20-100 
-#define COOLING  55
+#define COOLING  60
 
 // SPARKING: What chance (out of 255) is there that a new spark will be lit?
 // Higher chance = more roaring fire.  Lower chance = more flickery fire.
 // Default 120, suggested range 50-200.
-#define SPARKING 120
+#define SPARKING 50
 
+
+	virtual void init()
+	{
+		BaseAnimation::init();
+		FastLED.setBrightness(32);
+	}
 
 	virtual millis_t run()
 	{
@@ -510,7 +516,7 @@ public:
 	    	pixelnumber = j;
 	    	leds_[pixelnumber] = color;
 	    }
-	    return 1000/60;
+	    return 1000/10;
 	}
 };
 
